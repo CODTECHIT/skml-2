a5f10e689fd870b9336eff15524120ed4fe1c7df
mahajanashok2456 <ashokroshan78@gmail.com>
2026-06-26 12:15:24 +0530

    feat: auto-seed admin user

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
 import app from "../src-backend/app";
 import { connectDB } from "../src-backend/config/db";
 import { connectCloudinary } from "../src-backend/config/cloudinary";
+import { seedAdmin } from "../src-backend/utils/seedAdmin";
 
 // Ensure database and Cloudinary are initialized once per cold start
 connectDB();
 connectCloudinary();
+seedAdmin();
 
 export default app;
